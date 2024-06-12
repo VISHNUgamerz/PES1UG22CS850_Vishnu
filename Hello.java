8b513220adb35c23a0f4df07098e1cd1e927a330
Vishnu V <108284885+VISHNUgamerz@users.noreply.github.com>
2024-06-12 11:47:39 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World! PES1UG22CS850 COME ");
+        System.out.println("Hello, World! PES1UG22CS850 COME B");
     }
 }
